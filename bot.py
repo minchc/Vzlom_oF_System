c9de85e0fcd469067c1ca0b95b085138334d461a
Danya <minchc123@gmail.com>
2025-08-13 01:19:45 +0600

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,22 @@
+from aiogram import Bot, Dispatcher, types
+import asyncio
+
+TOKEN = "8343837798:AAFSRbHFKLGjA6l9mlQB9Nk1-62WfKmAToE"
+GAME_SHORT_NAME = "vzlom"  # тот самый, что указывали в BotFather
+GAME_URL = "https://minchc.github.io/Vzlom_oF_System/"  # ссылка на GitHub Pages
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def start_game(message: types.Message):
+    await message.answer_game(GAME_SHORT_NAME)
+
+@dp.callback_query_handler(lambda c: c.game_short_name == GAME_SHORT_NAME)
+async def game_callback(callback_query: types.CallbackQuery):
+    await bot.answer_callback_query(callback_query.id, url=GAME_URL)
+
+async def main():
+    await dp.start_polling()
+
+asyncio.run(main())
